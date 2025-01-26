1a5d0b57681cf1f8ad63ed762b0ebed027eb616b
Pratichi <46370317+FnuPratichi@users.noreply.github.com>
2025-01-25 22:08:15 -0500

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -34,7 +34,7 @@ const greeting = {
 
 const socialMediaLinks = {
   github: "https://github.com/FnuPratichi",
-  linkedin: "https://www.linkedin.com/in/pratichi11/",
+  linkedIn: "https://www.linkedin.com/in/pratichi11/",
   email: "pratichi.lnu@gwu.edu",
   leetcode: "https://leetcode.com/u/prat_noob/",
   medium: "https://medium.com/@pratichi3",
@@ -63,24 +63,24 @@ https://fontawesome.com/icons?d=gallery */
 
   softwareSkills: [
     {
-      skillName: "Python",
-      fontAwesomeClassname: "fab fa-html5"
+      skillName: "python",
+      fontAwesomeClassname: "fab fa-python"
     },
     {
-      skillName: "SQL",
+      skillName: "sql-database",
+      fontAwesomeClassname: "fas fa-database"
+    },
+    {
+      skillName: "CSS",
       fontAwesomeClassname: "fab fa-css3-alt"
     },
     {
       skillName: "Html-5",
-      fontAwesomeClassname: "fab fa-sass"
+      fontAwesomeClassname: "fab fa-html5" 
     },
     {
       skillName: "JavaScript",
       fontAwesomeClassname: "fab fa-js"
-    },
-    {
-      skillName: "CSS",
-      fontAwesomeClassname: "fab fa-react"
     },
     {
       skillName: "Flask",
@@ -91,24 +91,8 @@ https://fontawesome.com/icons?d=gallery */
       fontAwesomeClassname: "fab fa-swift"
     },
     {
-      skillName: "npm",
-      fontAwesomeClassname: "fab fa-npm"
-    },
-    {
-      skillName: "sql-database",
-      fontAwesomeClassname: "fas fa-database"
-    },
-    {
       skillName: "aws",
       fontAwesomeClassname: "fab fa-aws"
-    },
-    {
-      skillName: "firebase",
-      fontAwesomeClassname: "fas fa-fire"
-    },
-    {
-      skillName: "python",
-      fontAwesomeClassname: "fab fa-python"
     },
     {
       skillName: "docker",
@@ -128,18 +112,19 @@ const educationInfo = {
       logo: require("./assets/images/gwu_logo.png"),
       subHeader: "Master of Science in Computer Science",
       duration: "January 2024 - May 2025",
-      desc: "Participated in the research of XXX and published 3 papers.",
+      desc: "Courseworks : Design and Analysis of Algorithm, DBMS1, DBMS2, Cloud Computing, Linux for DevOps, Machine Learning, Data Mining",
       descBullets: [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        "SEAS Merit award receipnt",
+        ""
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
       ]
     },
     {
-      schoolName: "Stanford University",
+      schoolName: "Savitribai Phule Pune University",
       logo: require("./assets/images/stanfordLogo.png"),
       subHeader: "Bachelor of Science in Computer Science",
-      duration: "September 2013 - April 2017",
-      desc: "Ranked top 10% in the program. Took courses about Software Engineering, Web Security, Operating Systems, ...",
+      duration: "July 2016 - October 2020",
+      desc: "Ranked top 2% in the class. Took courses about Software Engineering, Algorithms, Web Security, Operating Systems, ...",
       descBullets: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit"]
     }
   ]
@@ -370,15 +355,14 @@ const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+92-0000000000",
-  email_address: "saadpasta70@gmail.com"
+  email_address: "pratichi.lnu@gwu.edu"
 };
 
 // Twitter Section
 
 const twitterDetails = {
   userName: "twitter", //Replace "twitter" with your twitter username without @
-  display: true // Set true to display this section, defaults to false
+  display: false // Set true to display this section, defaults to false
 };
 
 const isHireable = false; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
